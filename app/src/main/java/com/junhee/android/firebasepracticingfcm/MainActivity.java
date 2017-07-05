810fc3b2216e9e0ed422e30dafdd4174903ee58b
JunHee Lee <jhlee910609@gmail.com>
2017-07-06 00:35:51 +0900

    upload all files.

--- app/src/main/java/com/junhee/android/firebasepracticingfcm/MainActivity.java
+++ app/src/main/java/com/junhee/android/firebasepracticingfcm/MainActivity.java
@@ -62,16 +62,21 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.IPa
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    // 파이어베이스 DB에 있는 data들을 불러오기 위해 함수를 만들었다.
     private void setFirebaseListener() {
 
         uidRef.addValueEventListener(new ValueEventListener() {
+            // DataSanpshot 객체는 파이어베이스 내부에 존재하는 데이터를 언제든 읽을 수 있도록 설계된 instance이다.
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 List<Uid> list = new ArrayList<Uid>();
+                // dataSnapshot.getChildren()의 경우 배열에서 꺼내는 것처럼 하나씩 꺼내서
+                // Uid 객체를 만들고, 만들어진 Uid 객체를 list 배열에 저장한다.
                 for (DataSnapshot item : dataSnapshot.getChildren()) {
                     Uid uid = item.getValue(Uid.class);
                     list.add(uid);
                 }
+                // 그리고 아래 만든 refreshData(list); 함수를 통해 adapter에 데이터 최신화를 시켜준다.
                 refreshData(list);
             }
 
@@ -87,12 +92,11 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.IPa
     private void refreshData(List<Uid> data) {
         adapter.setData(data);
         adapter.notifyDataSetChanged();
-
     }
 
     @Override
     public void setToken(String token) {
-        textReceiver.setText(token);
+        textReceiver.setText("메시지 수신자의 토큰 = " + token);
 
     }
 
